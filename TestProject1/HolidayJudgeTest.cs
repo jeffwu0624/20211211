38ec4de6a646a2c2a576f023c347372a73837f87
jeffwu0624 <jeffwu0624@gmail.com>
2021-12-10 23:52:37 +0800

    create UserCreator

---
+++ TestProject1/HolidayJudgeTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NUnit.Framework;
+using WebApplication1;
+
+namespace TestProject1
+{
+    public class HolidayJudgeTest
+    {
+        [TestCase("2021/11/11")]
+        public void IsCelebrateDay_shouldReturnTrue(string value)
+        {
+            var holidayJudge = new FakeHolidayJudge()
+            {
+                Today = DateTime.Parse(value)
+            };
+
+            var isCelebrateDay = holidayJudge.IsCelebrateDay();
+
+            Assert.AreEqual(true, isCelebrateDay);
+        }
+    }
+
+    public class FakeHolidayJudge : HolidayJudge
+    {
+        public DateTime Today { get; set; }
+
+        public override DateTime GetToday()
+        {
+            return Today;
+        }
+    }
+}
--- WebApplication1/HolidayJudge.cs
+++ WebApplication1/HolidayJudge.cs
@@ -4,12 +4,17 @@
     {
         public bool IsCelebrateDay()
         {
-            if (DateTime.Today == DateTime.Parse("2021/11/11"))
+            if (GetToday() == DateTime.Parse("2021/11/11"))
             {
                 return true;
             }
 
             return false;
+        }
+
+        public virtual DateTime GetToday()
+        {
+            return DateTime.Today;
         }
     }
 }
