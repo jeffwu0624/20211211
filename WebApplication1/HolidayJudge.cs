563d80c96d5b8c3572739bfe0d703521c8cead97
jeffwu0624 <jeffwu0624@gmail.com>
2021-12-10 23:09:19 +0800

    create HolidayJudge class

---
+++ WebApplication1/HolidayJudge.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1
+{
+    public class HolidayJudge
+    {
+        public bool IsCelebrateDay()
+        {
+            if (DateTime.Today == DateTime.Parse("2021/11/11"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
